36f076235799d872f81e5905523b75f07636da7c
Noah Breanne Field <fieldn@data.cs.purdue.edu>
2014-11-02 03:18:31 -0500

    Initial Commit

---
+++ backup/links.js
@@ -0,0 +1,11 @@
+/* Just update the top header */
+function header(str) {
+    var strNew='top_'+str+'.html'
+    var strFrameset='frameset_'+str+'.html'
+    if (!(parent.header.location)) {
+	top.location.replace(strFrameset)
+	return
+    }
+
+    if (parent.header.location.href!=strNew) parent.header.location.replace(strNew)
+}
